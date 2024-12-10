1d4f74dd0d0ae2f3962ae972c26bf77d13011470
elanurozmen <200201110@kocaeli.edu.tr>
2024-12-10 11:11:34 +0300

    Trigger test commit

--- Api_Project/Api/Controller/MongoController.cs
+++ Api_Project/Api/Controller/MongoController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
-
 [ApiController]
 [Route("api/[controller]")]
 public class MongoController : ControllerBase
--- Api_Project/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api_Project/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71b313609eca7e1f7177901227c6f009cd96a086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74fcbef1d79347389cc14d1ae9055f7a15639442")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
