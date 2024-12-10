c35007bd3b883f96cf918ae8a1189f23b5555444
elanurozmen <200201110@kocaeli.edu.tr>
2024-12-10 14:16:35 +0300

    bad commit

--- Api_Project/Api/Controller/MongoController.cs
+++ Api_Project/Api/Controller/MongoController.cs
@@ -8,7 +8,7 @@ namespace Api.Controllers;
 [Route("api/[controller]")]
 public class MongoController : ControllerBase
 {
-    private readonly CheckService _checksService;
+    privat readonly CheckService _checksService;
 
     public MongoController(CheckService checksService) =>
         _checksService = checksService;
